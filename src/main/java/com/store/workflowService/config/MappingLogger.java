a6087ffd6bda329751374966d5fa8d9bde9ae2fd
KarenL19 <karen-_-19@outlook.com>
2026-03-10 21:12:02 -0300

    integracao com lambda

--- src/main/java/com/store/workflowService/config/MappingLogger.java
+++ src/main/java/com/store/workflowService/config/MappingLogger.java
@@ -2,23 +2,29 @@ package com.store.workflowService.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import java.util.List;
+
 @Component
 public class MappingLogger {
     private static final Logger log = LoggerFactory.getLogger(MappingLogger.class);
 
-    @Autowired
-    private RequestMappingHandlerMapping mapping;
+    private final List<RequestMappingHandlerMapping> mappings;
+
+    public MappingLogger(List<RequestMappingHandlerMapping> mappings) {
+        this.mappings = mappings;
+    }
 
     @EventListener(ApplicationReadyEvent.class)
     public void logMappings() {
-        log.info("Listing {} registered request mappings:", mapping.getHandlerMethods().size());
-        mapping.getHandlerMethods().forEach((k, v) -> log.info("Mapped {} -> {}", k, v));
+        int total = mappings.stream().mapToInt(m -> m.getHandlerMethods().size()).sum();
+        log.info("Listing {} registered request mappings:", total);
+        for (RequestMappingHandlerMapping m : mappings) {
+            m.getHandlerMethods().forEach((k, v) -> log.info("Mapped {} -> {}", k, v));
+        }
     }
 }
-
